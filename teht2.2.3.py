1c993f5e8bd069f78799fc8b718798231b2e2151
samwise50000 <sami.mattila3@metropolia.fi>
2022-08-22 14:46:26 +0300

    Tehtävä 2.2.2

---
+++ teht2.2.3.py
@@ -0,0 +1,17 @@
+#Kysytään tiedot
+kanta = float(input('Anna suorakulmion kanta: '))
+korkeus = float(input('Anna suorakulmion korkeus: '))
+
+#Laskutoimitukset
+piiri = kanta+kanta+kanta+kanta
+ala = kanta * korkeus
+
+#Tulostukset
+print("Piiri on: %.2f" %piiri)
+print("Ala on: %.2f" %ala)
+
+# A = kanta * korkeus
+# piiri = (kanta * 2) + (korkeus * 2)
+
+#print f(f"ala = {a}")
+#print(f"piiri = {piiri}")
