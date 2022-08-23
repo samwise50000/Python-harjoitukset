1001b94c4f96813c3554caa2e78582f836126ba2
samwise50000 <sami.mattila3@metropolia.fi>
2022-08-23 09:43:08 +0300

    Tehtävä 2.2.5

--- teht2.2.5.py
+++ teht2.2.5.py
@@ -1,6 +1,25 @@
-Leiviskät
-Naulat
-Luodit
+#Arvot
+luoti = (13.3)
+naula = (32 * luoti)
+leiviskä = (20 * naula)
+
+#Kysytään tiedot
+arvo1lei = float(input('Anna leiviskät: '))
+arvo2nau = float(input('Anna naulat: '))
+arvo3luo = float(input('Anna luodit: '))
+grammayht = luoti * arvo3luo + naula * arvo2nau + leiviskä * arvo1lei
+#kilot = gramma / 1000
+
+kg = grammayht // 1000
+grammayht2 = grammayht % 1000
+
+#Laskutoimitus
+
+print(f"Massa nykymittojen mukaan: \n{kg} kilogrammaa ja {grammayht2:.2f} grammaa.")
+
+
+
+
 
 #Laske yllämainittujen muuttujien yhteis gramma
 #Pitää tulostaa monta kg ja grammaa
@@ -10,3 +29,5 @@ Luodit
 #Halutaan kokonaislukuna kilot, ei desimaalia
 #math.floor komento. Pyöristää luvut alaspäin, käytä tätä kun jaat summan 1000 niin saat kilot.
 #Lopuksi laske vielä jämä grammat
+
+#Kaksi // merkkiä unohtaa desimaalit
