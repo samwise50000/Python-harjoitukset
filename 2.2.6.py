3a1885c7d1cabdaeab4c676b5d27888d1683463b
samwise50000 <sami.mattila3@metropolia.fi>
2022-08-23 10:08:42 +0300

    Tehtävä 2.2.6

--- 2.2.6.py
+++ 2.2.6.py
@@ -3,5 +3,13 @@
 import random
 
 x = random.randint(0, 9)
-print(x)
+y = random.randint(0, 9)
+z = random.randint(0, 9)
+print(x,y,z)
+
+e = random.randint(1, 6)
+r = random.randint(1, 6)
+s = random.randint(1, 6)
+a = random.randint(1, 6)
+print(e,r,s,a)
 
